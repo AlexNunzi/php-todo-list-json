a3674f10e35b93d7f7aa2f005867ae427ccae477
Alex Nunzi <alex.nunzi@hotmail.it>
2023-04-20 16:14:58 +0200

    Bonus: Creato file todolist.json all'interno del quale è salvata la lista delle cose da fare e dalla quale vengono lette e scritte le informazioni tramite file_get_contents e file_put_contents

--- server.php
+++ server.php
@@ -1,14 +1,10 @@
 <?php
 
-$todoList = [
-    'Fare la spesa',
-    'Andare dal dentista',
-    'Stendere i panni',
-    'Preparare la cena'
-];
+$todoList = json_decode(file_get_contents('todolist.json'), true);
 
 if(isset($_POST['newTask']) && !empty($_POST['newTask'])){
     $todoList[] = $_POST['newTask'];
+    file_put_contents('todolist.json', json_encode($todoList));
 }
 
 header('Content-Type: application/json');
